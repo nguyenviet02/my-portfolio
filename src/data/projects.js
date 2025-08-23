ff865cc3c443177d24478b0b9b5696e2e9219b20
Nguyen Viet <nguyenviet02.dev@gmail.com>
2025-08-23 22:36:32 +0700

    feat: add project images

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,14 +5,15 @@ const projects = [
     date: "April, 2022",
     projectType: "Web Development",
     title: "E-learning Platform",
-    description: `Just launched a new e-learning platform built with React, Node.js and MongoDB. <a href="https://tek4.vn" target="_blank">tek4.vn</a>`,
+    description: `This is E-learning Platform where you can learn and practice programming languages, AI, DSA and many more. <br />
+	<b>URL:</b> <a href="https://tek4.vn" target="_blank">https://tek4.vn</a>`,
     images: [
       "/images/projects/tek4/tek4.png",
       "/images/projects/tek4/tek4-3.png",
       "/images/projects/tek4/tek4-4.png",
       "/images/projects/tek4/tek4-2.png",
     ],
-    technologies: ["React", "Node.js", "Express", "MongoDB", "Stripe"],
+    technologies: ["Next.js", "TypeScript", "SCSS", "Shadcn UI", "Ant Design", "GraphQL"],
     likes: 24,
     comments: 5,
     shares: 2,
